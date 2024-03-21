29dc53ea6428bd5869d0f8cc733cc7befb0a8345
fagirton <fagirton2015@gmail.com>
2024-03-21 19:08:19 +0300

    Commenting and small beautifying

--- p3-player.ino
+++ p3-player.ino
@@ -40,18 +40,18 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Structs
 struct AnimSeq { // A struct for intercepting every animation at the same time synced with 60hz timer
-  void (*renderFunc)(struct AnimSeq *self);
-  uint8_t step;
-  uint8_t duration; // duration in 60 frames per second (interpolated, if FPS is set to less than that)
-  int16_t offset[2];
+  void (*renderFunc)(struct AnimSeq *self); // A function that executes every frame of animation. The whole AnimSeq object is considered empty if this pointer equals NULL. @note However, calling this function when it's NULL may cause instant crash, be careful.
+  uint8_t step; // Current frame of animation (gets updated with every frame)
+  uint8_t duration; // Duration in 60 frames per second (interpolated, if FPS is set to less than that)
+  int16_t offset[2]; // Should be used to offset drawing current animation if whole object moves
 };
 
 struct MusicState { // A struct defining current state of playing track 
-  char *trackName; //if not working try []
-  byte PlayState; // That icon close to the timer
-  uint16_t timer; // I need more digits!!!
-  uint8_t volume;
-  uint8_t battery;
+  char *trackName; // Name of the track currently playing (if not working try [])
+  byte PlayState; // That icon close to the timer, shows the status of playing (Play, Pause, Stop)
+  uint16_t timer; // A timer of current song. Note that it uses uint16 (I need more digits!!!)
+  uint8_t volume; // Current volume of player from 0 to 5
+  uint8_t battery; // Current battery level of player from 0 to 4
 } MusicState;
 
 
@@ -59,7 +59,6 @@ struct AnimSeq *AnimQueue[4];
 
 void setup() {
   Serial.begin(9600); //debug Serial with PC
-
   for (uint8_t i = 0; i < 4; i++) {
     AnimQueue[i] = (struct AnimSeq*)malloc(sizeof(struct AnimSeq));
     AnimQueue[i]->renderFunc = NULL;
@@ -78,73 +77,29 @@ void setup() {
   display.setTextColor(BLACK); 
   display.cp437(true);         // Use full 256 char 'Code Page 437' font
 
-  Serial.println(F("testing serial"));
-
-  
-  Serial.println(isEmpty());
-  Serial.println(getFirstEmptyAnim());
-  /*
   // Menu testing snippet
-  
-  // render corner with name
-  display.fillTriangle(0,0, 0,32,16,32, BLACK);
-  display.setRotation(3); //rotates text on OLED 1=90 degrees, 2=180 degrees
-  display.setCursor(0, 0);
-  display.setTextColor(WHITE);
-  display.print(F("MAIN"));
-  display.setRotation(0);
-
-  uint8_t winX = 16;
-  uint8_t winY = 10;
-  uint8_t winWidth = 96;
-  uint8_t winHeight = 11;
-
-  // center option
-  display.fillRect(winX, winY, winWidth, winHeight, BLACK);
-  display.setCursor(winX + 2, winY + 2);
-  display.print(F("First option"));
-
-  // side options
-  display.setTextColor(BLACK);
-
-  display.setCursor(winX, winY - 8);
-  display.print((char)0x1E);
-  display.print(" ");
-  display.print("Second option");
-
-  display.setCursor(winX,winY + 12);
-  display.print((char)0x1F);
-  display.print(" ");
-  display.print("Third option");
-  */
+  drawMenuCorner("MAIN");
+  drawUpperOption("second");
+  drawCenterOption("center");
+  drawLowerOption("lower");
 
   // Scrolling test
   //display.startscrolldiagright(0x0F, 0x0F);
   //display.startscrollright(0x00, 0x64);
 
+  display.display(); // Wake up, boy
 
-  //display.display(); // Wake up, boy
-
-  //Serial.println("starting");
-  drawMediaBar(globalOffset);
-
-  display.display();
 
   // 60 hz timer
   cli();
   TCCR1A = 0x00; // Normal mode, => Disconnect Pin OC1 PWM Operation disabled
-  TCCR1B = 1 << CS11; // 16MHz clock with prescaler, TCNT1 increments every .5 uS (cs11 bit set)
+  TCCR1B = 0x02; // 16MHz clock with prescaler, TCNT1 increments every .5 uS (cs11 bit set)
 
   OCR1A = (unsigned int)(33333 * FPS); // = 16666 microseconds (each count is .5 us)
   TIMSK1 |= (1 << OCIE1A); // Enable interrupts
   sei();
-  
 
-  
-  //Serial.println(freeMemory());
-  //Serial.println(F("Updating"));
-  delay(1000);
-  Anim_SwitchSongs();
+  //Anim_SwitchSongs();
 }
 
 void check(bool val){
@@ -171,7 +126,13 @@ void loop() {
   display.display();
 }
 
-bool isEmpty(){
+/*!
+    @brief  Function that returns 0 when there's no animations in the queue and 1 elsewhere
+    @return Returns bool value
+            0 - no animations in the queue
+            1 - at least one animation is in the queue
+*/
+bool isEmpty() { // 
   for (uint8_t i = 0; i < 4; i++) {
     if (AnimQueue[i]->renderFunc != NULL) {
       return false;
@@ -180,6 +141,10 @@ bool isEmpty(){
   return true;
 }
 
+/*!
+    @brief  Function that returns first empty animation ready to be overwritten
+    @return Returns uint8 number with an index of animation between 0 and 3, and 4 if whole queue is full
+*/
 uint8_t getFirstEmptyAnim() {
   // 4 means AnimQueue is full
   for (uint8_t i = 0; i < 4; i++) {
@@ -190,6 +155,9 @@ uint8_t getFirstEmptyAnim() {
   return 4;
 }
 
+/*!
+    @brief  This function *deletes* every animation in the queue that is already finished
+*/
 void PopAnim() {
   for (uint8_t i = 0; i < 4; i++) {
     if (AnimQueue[i]->duration < AnimQueue[i]->step) {
@@ -198,7 +166,23 @@ void PopAnim() {
   }
 }
 
-// animation internal functions
+// Animation internal functions
+
+// Render function for Menu Corner sliding across the screen on menu open
+void renderAnim_MenuCornerSlide(struct AnimSeq *self) {
+  uint8_t travel = 112; //in pixels
+
+  if (self->step > self->duration) {
+    PopAnim();
+    return;
+  }
+
+  self->offset[1] = map(self->step, 0, self->duration, 0, travel);
+
+  self->step++;
+}
+
+// Render function for swiping out the current song
 void renderAnim_SwipeOutSong(struct AnimSeq *self) {
   uint8_t travel = 32; //in pixels
 
@@ -209,11 +193,10 @@ void renderAnim_SwipeOutSong(struct AnimSeq *self) {
 
   self->offset[1] = map(self->step, 0, self->duration, 0, travel);
   drawMediaBar(self->offset);
-  //Serial.print("OUT");
-  //Serial.println(self->offset[1]);
   self->step++;
 }
 
+// Render function for swiping in the next song
 void renderAnim_SwipeInSong(struct AnimSeq *self) {
   uint8_t travel = 32; //in pixels
 
@@ -224,11 +207,13 @@ void renderAnim_SwipeInSong(struct AnimSeq *self) {
 
   self->offset[1] = -map(self->step, 0, self->duration, travel, 0);
   drawMediaBar(self->offset);
-  //Serial.print("IN");
-  //Serial.println(self->offset[1]);
   self->step++;
 }
 
+/*!
+    @brief  Function for a "functional" animation which launches animation to switch songs in and out.
+    @note    There's no function to actually switch current song to another one
+*/
 void funcAnim_SwitchSongs(struct AnimSeq *self) {
   self->step++;
   if (self->step > self->duration) {
@@ -244,10 +229,25 @@ void funcAnim_SwitchSongs(struct AnimSeq *self) {
   if (self->step == 1) {
     Anim_SwipeOutSong();
   }
-
 }
 
 // animation methods
+
+// Function that initiates the animation for Menu Corner sliding across the screen on menu open
+void Anim_MenuCornerSlide() {
+  uint8_t multiplier = 60 / FPS;
+  uint8_t duration = 12; // durations of SwipeIn and SwipeOut + delay
+  uint8_t i = getFirstEmptyAnim();
+  if (i < 4) {
+    AnimQueue[i]->renderFunc = &renderAnim_MenuCornerSlide;
+    AnimQueue[i]->step = 0;
+    AnimQueue[i]->duration = duration / multiplier;
+    AnimQueue[i]->offset[0] = 0;
+    AnimQueue[i]->offset[1] = 0;
+  }
+}
+
+// Function that initiates the animation which launches animation to switch songs in and out.
 void Anim_SwitchSongs() {
   uint8_t multiplier = 60 / FPS;
   uint8_t duration = 12 + 6; // durations of SwipeIn and SwipeOut + delay
@@ -261,6 +261,7 @@ void Anim_SwitchSongs() {
   }
 }
 
+// Function that initiates the animation for swiping out the current song
 void Anim_SwipeOutSong() {
   uint8_t multiplier = 60 / FPS;
   uint8_t duration = 12;
@@ -274,6 +275,7 @@ void Anim_SwipeOutSong() {
   }
 }
 
+// Function that initiates the animation for swiping in the next song
 void Anim_SwipeInSong() {
   uint8_t multiplier = 60 / FPS;
   uint8_t duration = 12;
@@ -287,24 +289,48 @@ void Anim_SwipeInSong() {
   }
 }
 
-// Using built-in map() instead of this unfinished func
-//uint8_t LerpInt(uint8_t travel,uint8_t step, uint8_t duration){
-//  uint8_t diff = travel % duration;
-//  if (diff == 0) {
-//    return (32 / duration * step);
-//  } else if (travel < duration) {
-
-//  } else if (travel > duration){
-//    if (step == duration){
-//      return (32 / duration * step) + diff
-//    } else {
-//      return (32 / duration * step)
-//    }
-//  }
-//}
-
-
 // draw methods, todo: redrawing if already drawn smth
+
+  // options
+  // kinda need to move this to define or const
+const uint8_t winX = 16;
+const uint8_t winY = 10;
+const uint8_t winWidth = 96;
+const uint8_t winHeight = 11;
+
+void drawCenterOption(char * text){
+  // center option
+  display.setTextColor(WHITE);
+  display.fillRect(winX, winY, winWidth, winHeight, BLACK);
+  display.setCursor(winX + 2, winY + 2);
+  display.print(text);
+}
+
+void drawUpperOption(char * text){
+  display.setTextColor(BLACK);
+  display.setCursor(winX, winY - 8);
+  display.print((char)0x1E);
+  display.print(" ");
+  display.print(text);
+}
+
+void drawLowerOption(char * text){
+  display.setTextColor(BLACK);
+  display.setCursor(winX,winY + 12);
+  display.print((char)0x1F);
+  display.print(" ");
+  display.print(text);
+}
+
+void drawMenuCorner(char * text) {
+  display.fillTriangle(0, 0, 0, 32, 16, 32, BLACK);
+  display.setRotation(3); //rotates text on OLED 1=90 degrees, 2=180 degrees
+  display.setCursor(0, 0);
+  display.setTextColor(WHITE);
+  display.print(text);
+  display.setRotation(0);
+}
+
 
 // very debug, maybe not so sure
 void drawMediaBar(int16_t offset[2]){
@@ -403,84 +429,3 @@ void drawBattery(uint8_t level, int16_t offset[2]) {
   display.setCursor(offset[0] + startPos + 16 - 2, offset[1] +  2);
   display.write(0xF9);
 }
-
-
-// Please dont look down here
-/*
-  pinMode(buttonPause, INPUT_PULLUP);
-  pinMode(buttonNext, INPUT_PULLUP);
-  pinMode(buttonPrevious, INPUT_PULLUP);
-  pinMode(buttonVolumeUp, INPUT_PULLUP);
-  pinMode(buttonVolumeDown, INPUT_PULLUP);
-  mySoftwareSerial.begin(9600);
-  Serial.begin(9600);
-  delay(1000);
-  Serial.println();
-  Serial.println("DFPlayer Mini Demo");
-  Serial.println("Initializing DFPlayer...");
-  if (!myDFPlayer.begin(mySoftwareSerial)) {
-    Serial.println("Unable to begin:");
-    Serial.println("1.Please recheck the connection!");
-    Serial.println("2.Please insert the SD card!");
-    while (true);
-  }
-  Serial.println(F("DFPlayer Mini online."));
-  myDFPlayer.setTimeOut(300);
-  //----Set volume----
-  myDFPlayer.volume(15); //Set volume value (0~30).
-  //----Set different EQ----
-  myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
-  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
-  myDFPlayer.play(1); //Воспроизведение первого mp3
-  isPlaying = true; // воспроизводим
-  Serial.println("Playing..");
-  //----Читать информацию----
-  Serial.println(myDFPlayer.readState()); //читать состояние mp3
-  Serial.println(myDFPlayer.readVolume()); //Текущая громкость
-  Serial.println(myDFPlayer.readEQ()); // читаем настройку эквалайзера
-  Serial.println(myDFPlayer.readFileCounts()); // читать все файлы на SD-карте
-  Serial.println(myDFPlayer.readCurrentFileNumber()); // текущий номер файла воспроизведения
-  }
-
-  void loop() {
-  if (digitalRead(buttonPause) == LOW) {
-    if (isPlaying) { // если было воспроизведение трека
-      myDFPlayer.pause(); // пауза
-      isPlaying = false; // пауза
-      Serial.println("Paused..");
-    } else {        // иначе
-      isPlaying = true; // воспроизводим
-      myDFPlayer.start(); //запускаем mp3 с паузы
-    }
-    delay(500);
-  }
-  if (digitalRead(buttonNext) == LOW) {
-    if (isPlaying) {
-      myDFPlayer.next(); //Next Song
-      Serial.println("Next Song..");
-    }
-    delay(500);
-  }
-  if (digitalRead(buttonPrevious) == LOW) {
-    if (isPlaying) {
-      myDFPlayer.previous(); //Previous Song
-      Serial.println("Previous Song..");
-    }
-    delay(500);
-  }
-  if (digitalRead(buttonVolumeUp) == LOW) {
-    if (isPlaying) {
-      myDFPlayer.volumeUp(); //Volume Up
-      Serial.println("Volume Up..");
-    }
-    delay(500);
-  }
-  if (digitalRead(buttonVolumeDown) == LOW) {
-    if (isPlaying) {
-      myDFPlayer.volumeDown(); //Volume Down
-      Serial.println("Volume Down..");
-    }
-    delay(500);
-  }
-  }
-*/
